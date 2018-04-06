5c07e507b2ccab00f7d37158643c1c186a6385a7
Andre Heß <hess.andre@gmx.net>
2018-04-06 21:36:39 +0200

    added: workspace and project prepared for pods to be installed

---
+++ Podfile
@@ -0,0 +1,21 @@
+
+platform :ios, '11.0'
+source 'https://github.com/CocoaPods/Specs.git'
+project './myClockRadio/myClockRadio.xcodeproj'
+
+use_frameworks!
+
+target "myClockRadio" do
+    pod 'Alamofire'
+end
+
+target "myClockRadioTests" do
+    pod 'Quick'
+    pod 'Nimble'
+end
+
+target "myClockRadioUITests" do
+    pod 'Quick'
+    pod 'Nimble'
+end
+
---
+++ myClockRadio/myClockRadio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myClockRadio
+//
+//  Created by Andre Heß on 06.04.18.
+//  Copyright © 2018 Andre Heß. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
